8178ed7f117a186ede388edca8dbd2a4456a7538
ysknk <31568245+ysknk@users.noreply.github.com>
2023-01-05 20:53:35 +0900

    Update .imageminrc.js

--- .imageminrc.js
+++ .imageminrc.js
@@ -1,23 +1,7 @@
-const imageminMozjpeg = require('imagemin-mozjpeg')
-const imageminPngquant = require('imagemin-pngquant')
-const imageminGifsicle = require('imagemin-gifsicle')
-const imageminSvgo = require('imagemin-svgo')
-
 module.exports = {
-  plugins: [
-    imageminPngquant({
-      quality: [0.5, 1.0]
-    }),
-    imageminMozjpeg({
-      quality: 85,
-      progressive: true
-    }),
-    imageminGifsicle(),
-    imageminSvgo({
-      plugins: [
-        { removeViewBox: false }
-      ]
-    })
-  ]
-}
+  cwd: "./src/images/",
+  src: "**/[!_]*.{jpg,png,gif,svg}",
+  dest: "./dist/",
 
+  webp: "\.(jpg|png)$"
+}
