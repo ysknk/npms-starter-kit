1e2a9c4d5319c0878bc4988d8bb6ded5a7df3b12
ysknk <31568245+ysknk@users.noreply.github.com>
2023-01-06 18:39:55 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,26 +2,19 @@ module.exports = {
   "env": {
     "browser": true,
     "es2021": true,
-    "node": true,
+    "node": true
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended"
-  ],
+  "extends": ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended", "plugin:storybook/recommended"],
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
     "ecmaVersion": 12,
     "sourceType": "module"
   },
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "rules": {
-  },
+  "plugins": ["@typescript-eslint"],
+  "rules": {},
   "settings": {
     "react": {
       "version": "detect"
     }
-  },
+  }
 };
